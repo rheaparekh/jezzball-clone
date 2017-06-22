28ae08cd0733aa7f538616b2efc537b18d4a8629
Rhea Parekh <rheaparekh12@gmail.com>
2017-06-22 13:26:28 +0530

    changed name to gameManager

--- skullranger/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url,include
-from django.contrib import admin
-
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^oauth/', include('social_django.urls', namespace='social')), 
-    url(r'', include ('GameApp.urls')),
-]
