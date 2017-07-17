44ad358fd191406a1e7fe7eb27cbd743fe6afc3b
Rhea Parekh <rheaparekh12@gmail.com>
2017-07-17 23:35:09 +0530

    changed database

--- gameManager/settings.py
+++ gameManager/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'gameManager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
+#}
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.facebook.FacebookOAuth2',
